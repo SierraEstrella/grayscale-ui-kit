d50b005342329b4037434b5cf99baa1035bb60ed
SierraEstrella <waitingbasil@gmail.com>
2021-01-07 17:17:28 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    './**/*.html',
+    './*.html',
+    '*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose
+    }
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      opacity: ['group-hover'],
+      transitionTimingFunction: ['group-hover'],
+      position: ['group-hover']
+    }
+  },
+  plugins: [],
+}
